5ab626b49f53b296b44ec5f4b3a0c823edef1ce9
Zach Bitterman <bitterman.8@osu.edu>
2025-10-14 17:39:43 -0400

    Simple Setup?

--- src/main.java
+++ src/main.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+import java.lang.String;
+import java.lang.System;
 public class main {
-
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Database System for Group 10");
+    }
 }
